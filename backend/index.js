bc95202adf5f93132c442896bc4a80fa62a94826
Raymondzyl <zhuyuleiraymond@yahoo.com>
2023-12-03 16:14:23 +1000

    Added message, profile modules

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
-//git init
-//npm init
-//npm i nodemon concurrently -D
-//npm i express cors json-server axios
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
+//create web server
+const app = express();
+
+app.use(cors());
+
+app.use('/api',router);
+
+const PORT = 80;
+app.listen(PORT, function(){
+    console.log('Server is running on http://localhost:80')
+})
---
+++ backend/routes/message.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const messageRouter = express.Router();
+const {getMessage} = require('../controllers/messageController')
+
+messageRouter.get('/', getMessage)
+
+module.exports = messageRouter;
