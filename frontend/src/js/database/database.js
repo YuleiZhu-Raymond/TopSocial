d0659338515bd48c520d5320da6c2821f3354bba
Raymondzyl <zhuyuleiraymond@yahoo.com>
2023-12-19 09:23:29 +1000

    changes on database.js and utils.js

--- frontend/src/js/database/database.js
+++ frontend/src/js/database/database.js
@@ -2,17 +2,15 @@ import axios from "axios";
 import { getPath } from "@/js/database/utils.js";
 
 const fetchData = async (endpoint) => {
-    try {
-        await axios.get(endpoint)
-        return data;
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    const data = await axios.get(endpoint)
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export const getDatabase = async () => {
-    
+export const getDatabase = async () => {  
     const [cards, profile, highline, message, request] = await Promise.all([
         (async () => (await fetchData(getPath('cards'))).data)(),
         (async () => (await fetchData(getPath('profile'))).data)(),
@@ -21,5 +19,5 @@ export const getDatabase = async () => {
         (async () => (await fetchData(getPath('request'))).data)(),
     ]);
 
-    console.log('here is the data we get', cards, profile, highline, message, request);
+    return { cards, profile, highline, message, request };
 }
